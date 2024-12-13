627b49200dab14e3d8c2778b1e64ab9a837f80e9
liuyanliang <2214888601@qq.com>
2024-12-13 16:56:01 +0800

    2024年12月13日  15点21分

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,12 +1,14 @@
 import { defineClientConfig } from 'vuepress/client'
-// import RepoCard from 'vuepress-theme-plume/features/RepoCard.vue'
-// import CustomComponent from './theme/components/Custom.vue'
+import Swiper from 'vuepress-theme-plume/features/Swiper.vue'
+import RepoCard from 'vuepress-theme-plume/features/RepoCard.vue'
+import CustomComponent from './theme/components/Custom.vue'
 
 // import './theme/styles/custom.css'
 
 export default defineClientConfig({
   enhance({ app }) {
-    // app.component('RepoCard', RepoCard)
-    // app.component('CustomComponent', CustomComponent)
+    app.component('RepoCard', RepoCard)
+    app.component('CustomComponent', CustomComponent)
+    app.component('Swiper', Swiper)
   },
 })
